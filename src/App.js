a5a1ba858db7cce80d092e63b4a738372ed708b8
tsubame_misa <chmi19013@g.nihon-u.ac.jp>
2020-12-07 10:50:53 +0900

    fin

---
+++ src/App.js
@@ -0,0 +1,131 @@
+import { useEffect, useState } from "react";
+import { fetchImages } from "./api";
+
+const Header = () => {
+  return (
+    <headr className="hero is-dark is-bold">
+      <div className="hero-body">
+        <div className="container">
+          <h1 className="title">Cute Dog Images</h1>
+        </div>
+      </div>
+    </headr>
+  );
+};
+
+const Image = (props) => {
+  return (
+    <div className="card">
+      <div className="card-image">
+        <figure className="image">
+          <img src={props.src} alt="cute dog" />
+        </figure>
+      </div>
+    </div>
+  );
+};
+
+const Loading = () => {
+  return <p>Loading...</p>;
+};
+
+const Gallery = (props) => {
+  const { urls } = props;
+
+  if (urls == null) {
+    return <Loading />;
+  }
+
+  return (
+    <div className="columns is-vcentered is-multiline">
+      {urls.map((url) => {
+        return (
+          <div key={url} className="column is-3">
+            <Image src={url} />
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+const Form = (props) => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const { breed } = event.target.elements;
+    props.onFormSubmit(breed.value);
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div className="field has-addons">
+          <div className="control is-expanded">
+            <div className="select is-fullwidth">
+              <select name="breed" defaultValue="shiba">
+                <option value="shiba">Shiba</option>
+                <option value="akita">Akita</option>
+              </select>
+            </div>
+          </div>
+          <div className="control">
+            <button type="submit" className="button is-dark">
+              Reload
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+const Main = () => {
+  const [urls, setUrls] = useState(null);
+  useEffect(() => {
+    fetchImages("shiba").then((urls) => {
+      setUrls(urls);
+    });
+  }, []);
+  const reloadImages = (breed) => {
+    fetchImages(breed).then((urls) => {
+      setUrls(urls);
+    });
+  };
+  return (
+    <main>
+      <section className="section"></section>
+      <div className="container">
+        <Form onFormSubmit={reloadImages} />
+      </div>
+      <section className="section">
+        <div className="container">
+          <Gallery urls={urls} />
+        </div>
+      </section>
+    </main>
+  );
+};
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="content has-text-centerd">
+        <p>Dog images are retrieved from Dog API</p>
+        <p>
+          <a href="https://dog.ceo/dog-api/about">Donate to Dog API</a>
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
